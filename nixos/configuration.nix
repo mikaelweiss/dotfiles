260972d40d1288263f4981a5712ae777026b267e
Mikael Weiss <campingmikael@icloud.com>
2026-01-05 13:18:16 -0700

    nvme and jellyfin

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -167,6 +167,13 @@
     options = [ "defaults" "nofail" "uid=1000" "gid=100" "umask=0022" ];
   };
 
+  # Mount the NVME
+  fileSystems."/mnt/nvme" = {
+    device = "/dev/disk/by-uuid/088fdaee-9d60-47ea-b70c-dcd81d5264cd";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
@@ -298,6 +305,8 @@
       ExecStart = "/var/lib/ilovemywife/i_love_my_wife/_build/prod/rel/i_love_my_wife/bin/i_love_my_wife start";
     };
   };
+  services.jellyfin.enable = true;
+  services.jellyfin.user = "mikaelweiss";
   
   # Virtualisations
   
@@ -326,7 +335,7 @@
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 4000 ];
+  # networking.firewall.allowedTCPPorts = [ 4000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
